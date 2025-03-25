4eeede870ece911cd909df5408efd3c889256d8e
MatheusConrado1 <matheus342conrado@gmail.com>
2025-03-25 15:24:32 -0300

    User entity and resource

---
+++ src/main/java/com/matheusconrado/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.matheusconrado.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.matheusconrado.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Joje", "jrg@gmail,.com", "99999999", "123123");
+		return ResponseEntity.ok().body(u);
+	}
+}
