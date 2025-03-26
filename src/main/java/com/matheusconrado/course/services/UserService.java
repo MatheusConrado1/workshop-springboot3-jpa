b548c0c468ebde50e6c9a26174d16408f5a0beaa
MatheusConrado1 <matheus342conrado@gmail.com>
2025-03-26 17:17:50 -0300

    Service layer, component registration

---
+++ src/main/java/com/matheusconrado/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.matheusconrado.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matheusconrado.course.entities.User;
+import com.matheusconrado.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
