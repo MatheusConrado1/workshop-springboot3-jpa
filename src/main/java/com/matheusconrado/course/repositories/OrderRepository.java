6f7532958a102d9cb6beacb2c89a80b6b1b94e35
MatheusConrado1 <matheus342conrado@gmail.com>
2025-03-27 10:26:31 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/matheusconrado/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.matheusconrado.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.matheusconrado.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
