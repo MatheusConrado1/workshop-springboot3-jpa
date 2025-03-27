cb6b77d8f10cfea5d0ee5d30a0d656f654903ab2
MatheusConrado1 <matheus342conrado@gmail.com>
2025-03-27 11:01:41 -0300

    Category

---
+++ src/main/java/com/matheusconrado/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.matheusconrado.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matheusconrado.course.entities.Category;
+import com.matheusconrado.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
