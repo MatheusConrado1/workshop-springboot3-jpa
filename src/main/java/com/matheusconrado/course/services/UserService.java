f27c9b88c03acd8cb8ddf82d21ed8546781d1794
MatheusConrado1 <matheus342conrado@gmail.com>
2026-03-26 08:37:40 -0300

    Exception handling - update

--- src/main/java/com/matheusconrado/course/services/UserService.java
+++ src/main/java/com/matheusconrado/course/services/UserService.java
@@ -3,6 +3,7 @@ package com.matheusconrado.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -44,9 +45,13 @@ public class UserService {
 	
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(e);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
